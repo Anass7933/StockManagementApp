3bcedfafaeec0562e74f2736c11c3c9d204e9619
khalid hida <khalid.hida@usmba.ac.ma>
2025-11-14 13:00:38 +0100

    modifying files

---
+++ src/main/java/com/stockapp/Main.java
@@ -0,0 +1,22 @@
+package com.stockapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/DefaultView.fxml"));
+        primaryStage.setTitle("hello world");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/stockapp/controllers/LoginController.java
@@ -0,0 +1,10 @@
+package com.stockapp.controllers;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+    @FXML
+    public void initialize() {
+        System.out.println("DefaultController loaded successfully!");
+    }
+}
