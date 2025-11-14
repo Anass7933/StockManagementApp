d984c3bf2d03b1c914f93500bef1f219e79182df
Anass7933 <anasseroua224@gmail.com>
2025-11-14 15:03:24 +0100

    setting up utils/DatabaseUtils.java

--- src/main/java/com/stockapp/utils/DatabaseUtils.java
+++ src/main/java/com/stockapp/utils/DatabaseUtils.java
@@ -6,11 +6,11 @@ public class DatabaseUtils {
 
     private static final String HOST = "localhost";
     private static final String PORT = "5432";
-    private static final String DATABASE = "mydb";
+    private static final String DATABASE = "stockdp";
     private static final String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE;
 
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "your_password";
+    private static final String USER = "user";
+    private static final String PASSWORD = "user1";
 
     private static final String DRIVER = "org.postgresql.Driver";
 
